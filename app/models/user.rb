81c8b1dd793e9aebbdcfd2242b18d61b047b2de7
Cezar Halmagean <cezar@mixandgo.com>
2022-03-31 15:04:08 +0300

    Added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  validates :email, presence: true, confirmation: true
+  validates :email_confirmation, presence: true
 
+  validates :name, format: { with: /\A[a-zA-Z]+\z/ },
+    length: { minimum: 3, maximum: 50 }
 end
